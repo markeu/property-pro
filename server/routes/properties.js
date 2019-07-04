07cc7e53f142fda435631d6e03ee409fc2dbb9e4
Uchey <ucheuzochukwumark@gmail.com>
2019-07-04 22:03:52 +0100

    feature(markAdvertSold):users can mark post as sold

--- server/routes/properties.js
+++ server/routes/properties.js
@@ -1,16 +1,18 @@
 import express from 'express';
 import PropertyController from '../controllers/PropertyController';
-import { postAdValidator, updateAdDataValidator } from '../middleware/propertyValidation';
+import { postAdValidator, updateAdStatusValidator, updateAdDataValidator } from '../middleware/propertyValidation';
 
 
 const router = express.Router();
 
 const {
-  createPropAd, updatePropertyAdData,
+  createPropAd, updatePropertyAdData, updatePropertyAdStatus,
 } = PropertyController;
+
 
 router.post('/', postAdValidator, createPropAd);
 router.patch('/:property-id', updateAdDataValidator, updatePropertyAdData);
+router.patch('/:property-id/sold', updateAdStatusValidator, updatePropertyAdStatus);
 
 
 export default router;
