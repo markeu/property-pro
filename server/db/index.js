855b5c31cdd2a2653bea3e613544444a1055975d
Uchey <ucheuzochukwumark@gmail.com>
2019-07-18 08:04:39 +0100

    feature(travis):establish flag  build on cloud database [Finishes #167270568]

--- server/db/index.js
+++ server/db/index.js
@@ -1,22 +1,15 @@
 /* eslint-disable no-undef */
 import { Pool } from 'pg';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
+
+
+dotenv.config();
 
 const pool = new Pool({
-	user: 'uche',
-	host: 'localhost',
-	database: 'property_pro',
-	password: '',
-	port: 5432,
-});
-
-// dotenv.config();
-
-// const pool = new Pool({
-// 	connectionString: process.env.DATABASE_URL
+	connectionString: process.env.DATABASE_URL
 	
-// });
+});
 
 export default pool;
 
